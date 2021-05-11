f96df4e6281341676db69e6fffa3a2b67e0020ed
ullerafaga <ullerafaga@gmail.com>
2021-05-10 22:09:39 -0300

    Add files via upload

---
+++ scripts/carga_incremental.R
@@ -0,0 +1,51 @@
+# script do professor ####
+library(dplyr)
+
+# carrega base de dados original
+chamadosTempoReal <- read.csv2('http://dados.recife.pe.gov.br/dataset/99eea78a-1bd9-4b87-95b8-7e7bae8f64d4/resource/079fd017-dfa3-4e69-9198-72fcb4b2f01c/download/sedec_chamados_tempo_real.csv', sep = ';', encoding = 'UTF-8')
+
+chamadosTempoReal <- chamadosTempoReal[-3,]
+
+# carrega base de dados para atualização
+chamadosTempoRealNew <- read.csv2('http://dados.recife.pe.gov.br/dataset/99eea78a-1bd9-4b87-95b8-7e7bae8f64d4/resource/079fd017-dfa3-4e69-9198-72fcb4b2f01c/download/sedec_chamados_tempo_real.csv', sep = ';', encoding = 'UTF-8')
+
+# compara usando a chave primária
+chamadosTempoRealIncremento <- (!chamadosTempoRealNew$processo_numero %in% chamadosTempoReal$processo_numero)
+
+# compara usando a chave substituta
+# criar a chave substituta
+chamadosTempoReal$chaveSubstituta = apply(chamadosTempoReal[, c(4,5)], MARGIN = 1, FUN = function(i) paste(i, collapse = ""))
+
+chamadosTempoRealNew$chaveSubstituta = apply(chamadosTempoRealNew[, c(4,5)], MARGIN = 1, FUN = function(i) paste(i, collapse = ""))
+
+# cria base de comparação
+chamadosTempoRealIncremento <- (!chamadosTempoRealNew$chaveSubstituta %in% chamadosTempoReal$chaveSubstituta)
+
+# comparação linha a linha
+setdiff(chamadosTempoRealNew, chamadosTempoReal)
+
+# retorna vetor com incremento
+chamadosTempoReal[chamadosTempoRealIncremento,]
+
+# junta base original e incremento
+chamadosTempoReal <- rbind(chamadosTempoReal, chamadosTempoReal[chamadosTempoRealIncremento,])
+
+# exercicio e resolução ####
+# Compartilhe com a gente um código criado por você utilizando um dos três métodos em um conjunto de dados diferente daquele do exercício. Lembre-se de compartilhar um link do github!!!
+
+#baixando o banco de dados
+vistoriasTempoReal <- read.csv2('http://dados.recife.pe.gov.br/dataset/99eea78a-1bd9-4b87-95b8-7e7bae8f64d4/resource/73b79738-9e76-4d3d-a057-625bb62b4bba/download/sedec_vistorias_tempo_real.csv', sep = ';', encoding = 'UTF-8')
+
+View(vistoriasTempoReal)
+
+vistoriasTempoReal <- vistoriasTempoReal[-1,] #tirei o uico caso que tinha para poder incrementar
+
+View(vistoriasTempoReal)
+
+# carrega base de dados para atualização
+vistoriasTempoRealNew <- read.csv2('http://dados.recife.pe.gov.br/dataset/99eea78a-1bd9-4b87-95b8-7e7bae8f64d4/resource/73b79738-9e76-4d3d-a057-625bb62b4bba/download/sedec_vistorias_tempo_real.csv', sep = ';', encoding = 'UTF-8')
+
+# compara usando a chave primária
+vistoriasTempoRealIncremento <- (!vistoriasTempoRealNew$vistoria_data %in% vistoriasTempoReal$vistoria_data)
+
+View(vistoriasTempoRealIncremento)
