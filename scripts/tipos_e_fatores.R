3d0be7c42a6f70e4c9f22f89f8cd5a5f6b2efe52
ullerafaga <ullerafaga@gmail.com>
2021-06-07 22:59:48 -0300

    Add files via upload

---
+++ scripts/tipos_e_fatores.R
@@ -0,0 +1,38 @@
+# script do professor
+# Códigos do livro An Introduction to data cleaning with R
+
+class(c("abc", "def"))
+class(1:10)
+class(c(pi, exp(1)))
+class(factor(c("abc", "def")))
+
+vrNumeric <- c("7", "7*", "7.0", "7,0")
+is.numeric(vrNumeric)
+as.numeric(vrNumeric)
+as.integer(vrNumeric)
+as.character(vrNumeric)
+
+is.na(as.numeric(vrNumeric))
+
+vrFactor <- factor(c("a", "b", "a", "a", "c"))
+levels(vrFactor)
+
+gender <- c(2, 1, 1, 2, 0, 1, 1)
+recode <- c(male = 1, female = 2)
+(gender <- factor(gender, levels = recode, labels = names(recode)))
+
+(gender <- relevel(gender, ref = "female"))
+
+age <- c(27, 52, 65, 34, 89, 45, 68)
+(gender <- reorder(gender, age))
+
+attr(gender, "scores") <- NULL
+gender
+
+# exercicio
+#  ▶ Compartilhe com a gente um código em que vocês cria uma estrutura de fatores.
+
+vrFactor_exercicio <- factor(c("b", "a", "b", "a", "b"))
+levels(vrFactor_exercicio)
+
+
