6664ae5ad01d28e310f50f985811b67265f7fa62
ullerafaga <ullerafaga@gmail.com>
2021-06-07 21:55:26 -0300

    Add files via upload

---
+++ scripts/estruturacao.R
@@ -0,0 +1,41 @@
+# script do professor ####
+library(data.table)
+library(dplyr)
+library(tidyverse)
+
+general_data<-fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_america_countries<-c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela") # vetor que identifica países latino americanos
+
+latin_america<- general_data %>% filter(location %in% latin_america_countries) # filtra casos apenas no vetor
+
+mlatin <- latin_america %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo nro de casos
+result <- mlatin %>% group_by(location) %>% filter(row == max(row))
+mlatin <- mlatin %>% filter(row<=min(result$row)) 
+
+# pivota o data frame de long para wide
+mlatinw <- mlatin %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location") 
+
+mlatin
+mlatinw
+
+# exercicio ####
+
+#Compartilhe com a gente um código em que você implementa um pivô long to wide ou wide to long
+
+EU_countries<-c("Austria", "Belgium", 'Bulgaria', "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden") # vetor que identifica países da união europeia
+
+EU_covid<- general_data %>% filter(location %in% EU_countries) # filtra casos apenas no vetor
+
+m_EU_covid <- EU_covid %>% group_by(location) %>% mutate(row = row_number()) %>% select(location, new_cases, row) # cria matriz dos países, agrupando por local, criando uma nova linha com index e selecionando apenas algumas variáveis
+
+# filtra dados para garantir que todos os países tenham mesmo nro de casos
+result <- m_EU_covid %>% group_by(location) %>% filter(row == max(row))
+m_EU_covid <- m_EU_covid %>% filter(row<=min(result$row)) 
+
+# pivota o data frame de long para wide
+m_EU_covidw <- m_EU_covid %>% pivot_wider(names_from = row, values_from = new_cases) %>% remove_rownames %>% column_to_rownames(var="location") 
+
+m_EU_covidw
